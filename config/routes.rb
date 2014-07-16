a8e424658a45be2cea36464efd539cd8dd2e0034
William B. Hauck <wbhauck@yahoo.com>
2014-07-16 03:18:14 -0400

    added Bootstrap css, javascript, and fonts; created welcome controller/view; started man navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ProjectUseful::Application.routes.draw do
+  get "welcome/index"
   resources :tasks
 
   resources :stories
@@ -23,7 +24,7 @@ ProjectUseful::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
