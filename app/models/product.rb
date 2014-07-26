1dbf269bfb0bd79fb14cea2852cf1e8f8b73bb81
William B. Hauck <wbhauck@yahoo.com>
2014-07-26 02:33:37 -0400

    http://www.projectuseful.org/stories/50 complete

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,5 @@ class Product < ActiveRecord::Base
   has_many :wikis
   has_many :wiki_pages
   belongs_to :status, class_name: "ProductStatusType", foreign_key: "product_status_type_id"
+  belongs_to :owner, class_name: "User", foreign_key: "owner_id"
 end
