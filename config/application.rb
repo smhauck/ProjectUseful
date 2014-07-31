06e62797e58a68ad230391a75d5ba8021c6d842f
William B. Hauck <wbhauck@yahoo.com>
2014-07-31 13:16:07 -0400

    http://www.projectuseful.org/stories/70 Blog posts and wiki pages need better formatting

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,10 @@ module ProjectUseful
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+  
+    config.action_view.sanitized_allowed_tags = 'table', 'thead', 'tbody', 'tfoot', 'tr', 'th', 'td'
+
+  
+  
   end
 end
