90a14147b7bd47aad4061d5cdb0ff47e0013c848
William B. Hauck <wbhauck@yahoo.com>
2015-08-25 20:49:13 -0400

    display projects without logging in

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,6 +17,7 @@
 
 
 class ProjectsController < ApplicationController
+  skip_before_action :authorize, only: [:index, :show]
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   # GET /projects
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,5 +1,5 @@
 module ProjectUseful
   class Application
-    VERSION = "0.5.6"
+    VERSION = "0.5.7"
   end
 end
