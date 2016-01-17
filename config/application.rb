4f80b91e885fcae734de64f2b0ff59b5fca5e6cc
William B. Hauck <wbhauck@yahoo.com>
2016-01-17 00:08:02 -0500

    allow paragraph tags

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module ProjectUseful
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   
-    config.action_view.sanitized_allowed_tags = 'a', 'b', 'br', 'em', 'emp', 'table', 'thead', 'tbody', 'tfoot', 'tr', 'th', 'td', 'ul', 'ol', 'li', 'h1','h2','h3','h4','h5','h6','strong', 'u'
+    config.action_view.sanitized_allowed_tags = 'a', 'b', 'br', 'em', 'emp', 'p', 'table', 'thead', 'tbody', 'tfoot', 'tr', 'th', 'td', 'ul', 'ol', 'li', 'h1','h2','h3','h4','h5','h6','strong', 'u'
 
  
   
