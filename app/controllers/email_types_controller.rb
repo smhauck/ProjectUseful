ec7825c9e354df35e01560a35385a0f5d0184aca
William B. Hauck <wbhauck@yahoo.com>
2018-01-31 01:06:41 -0500

    updated copyright to 2018

--- app/controllers/email_types_controller.rb
+++ app/controllers/email_types_controller.rb
@@ -1,3 +1,21 @@
+# Copyright (C) 2018 William B. Hauck, http://www.wbhauck.com
+# 
+# This file is part of Project Useful.
+# 
+# Project Useful is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Project Useful is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with Project Useful.  If not, see <http://www.gnu.org/licenses/>.
+
+
 class EmailTypesController < ApplicationController
   before_action :set_email_type, only: [:show, :edit, :update, :destroy]
 
